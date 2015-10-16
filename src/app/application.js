c1fadb333369fd806808597aa1946fa61694e59b
node-rookie <749924911@qq.com>
2015-10-16 12:04:25 +0800

    update

--- src/app/application.js
+++ src/app/application.js
@@ -2,10 +2,19 @@ var koa = require('koa');
 var app = module.exports = koa();
 var views = require('co-views');
 var logging = require('./logging');
+var logger = require('./logging').logger;
 var path = require('path');
-
-
+var settings = require('zeus-settings');
 var render= views(path.join(__dirname, '../views'), { map: { html: 'swig' }});
+app.env = 'development' || settings.env.NODE_ENV;
+app['port'] =  process.env.PORT || settings.env.PORT;//TODO: configure it by settings
+app['bindip'] =  process.env.BINDIP || settings.env.BINDIP;
+
+var http = require('http');
+var env = app.env;
+
+var system = require('./system');
+system.addMember('application', app);
 
 app.use(logging.generatorFunc);
 //router
@@ -21,6 +30,7 @@ app.on('error', function(err){
     console.log(err);
 })
 
-app.listen(3000, function(){
-    console.log('app listen on port: 3000');
+app.listen(app.port, app.bindip, function(){
+    logger.info('The server is binding on '+ app.bindip +' and listening on port ' + app.port + ' in ' + env );
+    system.memberUp(app);
 });
