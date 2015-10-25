2994c63ca37d595b1cd94b9d97b0c60ff2673906
node-rookie <749924911@qq.com>
2015-10-25 17:36:13 +0800

    update

--- src/controllers/api/customer.js
+++ src/controllers/api/customer.js
@@ -3,12 +3,16 @@ var userService = require('../../modules/user/services/UserService');
 var wechatUserService = require('../../modules/user/services/WechatUserService');
 var UserRole = require('../../modules/common/models/TypeRegistry').item('UserRole');
 var csState = require('../../modules/common/models/TypeRegistry').item('CSState');
+var userKvs = require('../../modules/user/kvs/User');
 var Promise = require('bluebird');
 
 module.exports = function(router) {
     router.get('/find', function*(){
         try{
             var customerList = yield userService.getRoleListAsync(UserRole.Customer.value());
+            for(var i = 0; i < customerList.length; i++){
+                customerList[i].activeTime = yield userKvs.loadSessionTTLByOpenidAsync(customerList[i].wx_openid);
+            }
             this.body = customerList;
         }catch(err){
             console.log('customer router find err:' + err);
